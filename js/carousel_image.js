f7b174a70cf53dbe4d9c93cb92f5283cf6df5da2
R3MANIAC <r3maniac>
2026-08-19 20:49:50 +0800

    HDC 2026 reveal

--- js/carousel_image.js
+++ js/carousel_image.js
@@ -6,7 +6,7 @@ function createCarouselImageTemplate() {
         <div class="carousel slide" data-ride="carousel">
             <div class="carousel-inner">
                 <div class="carousel-item active">
-                    <img src="img/banner_others.webp" class="d-block w-100" alt="Comic Fiesta 100 Days Countdown">
+                    <img src="img/banner_others.webp" style="max-height: 128px;" class="d-block w-100" alt="Comic Fiesta 100 Days Countdown">
                 </div>
             </div>
         </div>
--- js/dates_hdc.js
+++ js/dates_hdc.js
@@ -3,16 +3,16 @@ function loadDates() {
     elementUpdate("slot-reg-open-time", "9:00pm (UTC +8)");
 
     // Slot Registration Open Date
-    elementUpdate("slot-reg-open-date", "25th August 2025 (Monday)");
+    elementUpdate("slot-reg-open-date", "28th August 2026 (Friday)");
 
     // 10 Committee Selection Deadline
-    elementUpdate("deadline-10cs", "1st November 2025 (Saturday)");
+    elementUpdate("deadline-10cs", "1st November 2026 (Sunday)");
 
     // Wall Gallery Deadline
-    elementUpdate("deadline-wall-gallery", "30th November 2025 (Sunday)");
+    elementUpdate("deadline-wall-gallery", "30th November 2026 (Monday)");
 
     // Free Slot Submission Deadline
-    elementUpdate("deadline-fs", "7th December 2025 (Sunday)");
+    elementUpdate("deadline-fs", "6th December 2026 (Sunday)");
 }
 
 function elementUpdate(className, date) {
