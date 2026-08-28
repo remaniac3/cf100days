5a2863217f16d830fbd942225aad01fd70d15acb
R3MANIAC <r3maniac>
2026-08-28 19:39:12 +0800

    Open date change

--- js/dates_hdc.js
+++ js/dates_hdc.js
@@ -3,7 +3,7 @@ function loadDates() {
     elementUpdate("slot-reg-open-time", "9:00pm (UTC +8)");
 
     // Slot Registration Open Date
-    elementUpdate("slot-reg-open-date", "28th August 2026 (Friday)");
+    elementUpdate("slot-reg-open-date", "31st August 2026 (Monday)");
 
     // 10 Committee Selection Deadline
     elementUpdate("deadline-10cs", "1st November 2026 (Sunday)");
